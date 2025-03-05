a3f5de22e5a6e53f26a5eb71bb567872fa49ec9b
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2025-03-05 11:28:34 +0300

    feat(toggleterm): change mappings

--- config/plugins/ui/toggleterm.nix
+++ config/plugins/ui/toggleterm.nix
@@ -1,11 +1,65 @@
 {
-  config.vim.terminal.toggleterm = {
-    enable = true;
-    lazygit.enable = true;
-    mappings.open = "<leader>th";
-    setupOpts = {
-      winbar.enabled = false;
-      start_in_insert = true;
+  config.vim = {
+    terminal.toggleterm = {
+      enable = true;
+      lazygit.enable = true;
+      mappings.open = "<F7>";
+      setupOpts = {
+        winbar.enabled = false;
+        start_in_insert = true;
+      };
     };
+    keymaps = [
+      {
+        key = "<leader>tt";
+        mode = ["n"];
+        action = ":ToggleTerm direction=float<cr>";
+        silent = true;
+        desc = "Toggleterm float";
+      }
+      {
+        key = "<leader>th";
+        mode = ["n"];
+        action = ":ToggleTerm size=10 direction=horizontal<cr>";
+        silent = true;
+        desc = "Toggleterm horizontal split";
+      }
+      {
+        key = "<leader>tv";
+        mode = ["n"];
+        action = ":ToggleTerm size=80 direction=vertical<cr>";
+        silent = true;
+        desc = "Toggleterm vertical split";
+      }
+
+      {
+        key = "<C-h>";
+        mode = ["t"];
+        action = "<cmd>wincmd h<cr>";
+        silent = true;
+        desc = "Terminal left window navigation";
+      }
+      {
+        key = "<C-j>";
+        mode = ["t"];
+        action = "<cmd>wincmd j<cr>";
+        silent = true;
+        desc = "Terminal down window navigation";
+      }
+      {
+        key = "<C-k>";
+        mode = ["t"];
+        action = "<cmd>wincmd k<cr>";
+        silent = true;
+        desc = "Terminal up window navigation";
+      }
+      {
+        key = "<C-l>";
+        mode = ["t"];
+        action = "<cmd>wincmd l<cr>";
+        silent = true;
+        desc = "Terminal right window navigation";
+      }
+    ];
   };
 }
