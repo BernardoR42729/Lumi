7253d3af6bf036535b683b71a333131d67b55306
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2025-03-10 12:13:01 +0300

    feat(alpha): nixify

--- config/plugins/ui/alpha.nix
+++ config/plugins/ui/alpha.nix
@@ -2,6 +2,7 @@
   config.vim = {
     dashboard.alpha = {
       enable = true;
+      theme = null;
       layout = let
         padding = val: {
           type = "padding";
@@ -157,61 +158,7 @@
             hl_shortcut = "Keyword";
           };
         }
-        # (padding 3)
-        # {
-        #   opts = {
-        #     hl = "AlphaFooter";
-        #     position = "center";
-        #   };
-        #
-        #   type = "text";
-        #   val = [
-        #     "  Loaded X plugins  in Y ms  "
-        #     ".............................."
-        #   ];
-        # }
       ];
     };
-    # pluginRC.alpha =
-    #   /*
-    #   lua
-    #   */
-    #   ''
-    #      local alpha = require("alpha")
-    #      local dashboard = require("alpha.themes.dashboard")
-    #
-    #      -- Set header
-    #      dashboard.section.header.val = {
-    #       [[                                ]],
-    #       [[                                ]],
-    #       [[ ██▓     █    ██  ███▄ ▄███▓ ██▓]],
-    #       [[▓██▒     ██  ▓██▒▓██▒▀█▀ ██▒▓██▒]],
-    #       [[▒██░    ▓██  ▒██░▓██    ▓██░▒██▒]],
-    #       [[▒██░    ▓▓█  ░██░▒██    ▒██ ░██░]],
-    #       [[░██████▒▒▒█████▓ ▒██▒   ░██▒░██░]],
-    #       [[░ ▒░▓  ░░▒▓▒ ▒ ▒ ░ ▒░   ░  ░░▓  ]],
-    #       [[░ ░ ▒  ░░░▒░ ░ ░ ░  ░      ░ ▒ ░]],
-    #       [[  ░ ░    ░░░ ░ ░ ░      ░    ▒ ░]],
-    #       [[    ░  ░   ░            ░    ░  ]],
-    #      }
-    #
-    #      dashboard.section.header.opts.hl = "AlphaHeader"
-    #
-    #      -- Set menu
-    #      dashboard.section.buttons.val = {
-    #          dashboard.button( "n", "📄 New     ", ":ene <BAR> startinsert <CR>"),
-    #          dashboard.button( "r", "🌺 Recent  ", ":Telescope oldfiles<CR>"),
-    #          dashboard.button( "s", "🔎 Restore ", "<cmd>lua require('persistence').load({ last = true })<CR>"),
-    #          dashboard.button( "p", "💼 Projects", ":Telescope projects<CR>"),
-    #          dashboard.button( "q", "❌ Quit", "<cmd>exit<CR>"),
-    #      }
-    #
-    #     alpha.setup(dashboard.opts)
-    #
-    #      -- Disable folding on alpha buffer
-    #      vim.cmd([[
-    #          autocmd FileType alpha setlocal nofoldenable
-    #      ]])
-    #   '';
   };
 }
